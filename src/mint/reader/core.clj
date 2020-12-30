0bb9cf872da40e9d0c29257ed486dc83adb5d457
Gustavo Aguiar <gugahoa@gmail.com>
2020-12-30 03:43:28 -0300

    Create reader to be used at compile time

---
+++ src/mint/reader/core.clj
@@ -0,0 +1,39 @@
+(ns mint.reader.core (:require
+                      [clojure.java.io :as io]
+                      [clojure.string :as str]))
+
+(def directory (io/file "./posts"))
+(def files (filter #(.isFile %) (file-seq directory)))
+
+(defn- file-name [file]
+  (->> file
+       .toPath
+       .getFileName
+       str))
+
+(defn- raw-properties [reader]
+  (take-while not-empty reader))
+
+(defn- clean-raw-property [raw-property]
+  (-> raw-property
+      (str/split #":")
+      second
+      str/trim
+      (str/split #" " 2)
+      (update-in [0] keyword)))
+
+(defn build-properties [rdr]
+  (->> (line-seq rdr)
+       raw-properties
+       (map clean-raw-property)
+       (into {})))
+
+(defn- read-properties [file]
+  (with-open [rdr (io/reader file)]
+    (build-properties rdr)))
+
+(defmacro file-names []
+  (mapv file-name files))
+
+(defmacro posts-index []
+  (mapv read-properties files))
