6bb1a273dfce0f4e81694373326e2387caf10089
Gustavo Aguiar <gugahoa@gmail.com>
2020-12-31 03:35:02 -0300

    Rename subscription-form to subscription-button

--- src/mint/frontend/app.cljs
+++ src/mint/frontend/app.cljs
@@ -14,7 +14,7 @@
    [:div {:class "mt-3"}
     [:a {:href "#" :class "text-base font-semibold text-indigo-600 hover:text-indigo-500"} "Read full article"]]])
 
-(defn subscription-form []
+(defn subscription-button []
   [:div {:class "mt-6 flex flex-col sm:flex-row lg:mt-0 lg:justify-end"}
    [:div {:class "mt-2 flex-shrink-0 w-full flex rounded-md shadow-sm sm:mt-0 sm:w-auto sm:inline-flex"}
     [:a {:href "https://buttondown.email/gustavo.aguiar" :target "_blank" :class "w-full bg-indigo-600 px-4 py-2 border border-transparent rounded-md flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:inline-flex"} "Subscribe"]]])
@@ -26,7 +26,7 @@
      [:h2 {:class "text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl"} "Blog"]
      [:div {:class "mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center"}
       [:p {:class "text-xl text-gray-500"} "Get monthly articles in your inbox about functional programming languages"]
-      [subscription-form]]]
+      [subscription-button]]]
     [:div {:class "mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12"}
      (map make-article-summary (reader/posts-index))]]])
 
