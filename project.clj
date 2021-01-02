065ed72973b6a4f02cc9130acbede959989dba81
Ian Fernandez <d.ian.b@live.com>
2021-01-01 22:52:05 -0300

    Add exclusions

--- project.clj
+++ project.clj
@@ -3,10 +3,15 @@
   :license {:name "Eclipse Public License"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[reagent "1.0.0"]
-                 [thheller/shadow-cljs "2.11.11"]
                  [org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.773"]
-                 [com.google.javascript/closure-compiler-unshaded "v20200830"]]
+                 [org.clojure/clojurescript "1.10.773" :exclusions [com.google.code.findbugs/jsr305]]
+                 [com.google.javascript/closure-compiler-unshaded "v20200830" :exclusions [com.google.code.findbugs/jsr305]]
+                 [thheller/shadow-cljs "2.11.11" :exclusions [com.cognitect/transit-java
+                                                              commons-codec
+                                                              com.google.code.findbugs/jsr305
+                                                              org.clojure/tools.reader
+                                                              com.cognitect/transit-clj
+                                                              lorg.clojure/data.json]]]
   :profiles {}
   :java-source-paths ["src"]
   :jvm-opts ["-Djava.library.path=orgize-jni/target/debug:orgize-jni/target/release"]
